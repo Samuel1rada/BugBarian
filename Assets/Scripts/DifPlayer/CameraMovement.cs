044be4230d42bc34221b7bf6991b512e5e8c0f08
Samuel1rada <s.rada@student.alfa-college.nl>
2024-09-17 11:14:15 +0200

    better camera movement
    better camera movement still in development

--- Assets/Scripts/DifPlayer/CameraMovement.cs
+++ Assets/Scripts/DifPlayer/CameraMovement.cs
@@ -6,28 +6,26 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private Transform target;
-
     private Vector3 previousPos;
+
+    void Start()
+    {
+        previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
+        Cursor.visible = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
-        }
 
-        if(Input.GetMouseButton(0)) 
-        {
-            Vector3 dir = previousPos - cam.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 dir = previousPos - cam.ScreenToViewportPoint(Input.mousePosition);
 
-            cam.transform.position = target.position; //new Vector3();
+        cam.transform.position = target.position; //new Vector3();
 
-            cam.transform.Rotate(new Vector3(1, 0, 0), dir.y * 180);
-            cam.transform.Rotate(new Vector3(0, 1, 0), dir.x * 180, Space.World);
-            cam.transform.Translate(new Vector3(0,0,-10));
+        cam.transform.Rotate(new Vector3(1, 0, 0), dir.y * 180);
+        cam.transform.Rotate(new Vector3(0, 1, 0), dir.x * 180, Space.World);
+        cam.transform.Translate(new Vector3(0, 0, -4));
 
-
-            previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
-        }
+        previousPos = cam.ScreenToViewportPoint(Input.mousePosition);
     }
+
 }
